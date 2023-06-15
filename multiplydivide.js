d2e6e6392c377be131735f3ef0da65dc6f409099
ddarshit <darshitdayala007@gmail.com>
2023-06-15 17:00:03 +0530

    multiply&divide

---
+++ multiplydivide.js
@@ -0,0 +1,12 @@
+
+function multiply() {
+    number1 = document.getElementById("1stnumber").value
+    number2 = document.getElementById("2ndnumber").value
+    document.getElementById("result").innerHTML = number1 * number2;
+}
+function divide() {
+    number1 = document.getElementById("1stnumber").value
+    number2 = document.getElementById("2ndnumber").value
+    document.getElementById("result").innerHTML = number1 / number2
+
+}
